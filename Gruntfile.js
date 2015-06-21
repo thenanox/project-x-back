ca2feecdb92b7bbe38eada3b20c1e6c2f3b32fed
Fernando <fernando.rodriguez.adesis@gmail.com>
2015-06-21 17:03:53 +0200

    First version for backend wit REAN

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,210 +1,177 @@
-// Generated on 2015-06-13 using generator-socketio 0.0.3
 'use strict';
-var moment = require('moment');
 
-var LIVERELOAD_PORT = 35729;
-var RUNNING_PORT = 80; // <- if you change this, you need to change in public/js/app.js and recompile
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
+module.exports = function(grunt) {
+	// Unified Watch Object
+	var watchFiles = {
+		serverViews: ['app/views/**/*.*'],
+		serverJS: ['gruntfile.js', 'server.js', 'config/**/*.js', 'app/**/*.js'],
+		clientViews: ['public/modules/**/views/**/*.html'],
+		clientJS: ['public/js/*.js', 'public/modules/**/*.js'],
+		clientCSS: ['public/modules/**/*.css'],
+		mochaTests: ['app/tests/**/*.js']
+	};
 
-module.exports = function (grunt) {
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	// Project Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			serverViews: {
+				files: watchFiles.serverViews,
+				options: {
+					livereload: true
+				}
+			},
+			serverJS: {
+				files: watchFiles.serverJS,
+				tasks: ['jshint'],
+				options: {
+					livereload: true
+				}
+			},
+			clientViews: {
+				files: watchFiles.clientViews,
+				options: {
+					livereload: true,
+				}
+			},
+			clientJS: {
+				files: watchFiles.clientJS,
+				tasks: ['jshint'],
+				options: {
+					livereload: true
+				}
+			},
+			clientCSS: {
+				files: watchFiles.clientCSS,
+				tasks: ['csslint'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		jshint: {
+			all: {
+				src: watchFiles.clientJS.concat(watchFiles.serverJS),
+				options: {
+					jshintrc: true
+				}
+			}
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc',
+			},
+			all: {
+				src: watchFiles.clientCSS
+			}
+		},
+		uglify: {
+			production: {
+				options: {
+					mangle: false
+				},
+				files: {
+					'public/dist/application.min.js': 'public/dist/application.js'
+				}
+			}
+		},
+		cssmin: {
+			combine: {
+				files: {
+					'public/dist/application.min.css': '<%= applicationCSSFiles %>'
+				}
+			}
+		},
+		nodemon: {
+			dev: {
+				script: 'server.js',
+				options: {
+					nodeArgs: ['--debug'],
+					ext: 'js,html',
+					watch: watchFiles.serverViews.concat(watchFiles.serverJS)
+				}
+			}
+		},
+		'node-inspector': {
+			custom: {
+				options: {
+					'web-port': 1337,
+					'web-host': 'localhost',
+					'debug-port': 5858,
+					'save-live-edit': true,
+					'no-preload': true,
+					'stack-trace-limit': 50,
+					'hidden': []
+				}
+			}
+		},
+		ngAnnotate: {
+			production: {
+				files: {
+					'public/dist/application.js': '<%= applicationJavaScriptFiles %>'
+				}
+			}
+		},
+		concurrent: {
+			default: ['nodemon', 'watch'],
+			debug: ['nodemon', 'watch', 'node-inspector'],
+			options: {
+				logConcurrentOutput: true,
+				limit: 10
+			}
+		},
+		env: {
+			test: {
+				NODE_ENV: 'test'
+			},
+			secure: {
+				NODE_ENV: 'secure'
+			}
+		},
+		mochaTest: {
+			src: watchFiles.mochaTests,
+			options: {
+				reporter: 'spec',
+				require: 'server.js'
+			}
+		},
+		karma: {
+			unit: {
+				configFile: 'karma.conf.js'
+			}
+		}
+	});
 
-  grunt.initConfig({
+	// Load NPM tasks
+	require('load-grunt-tasks')(grunt);
 
-    cssmin: {
-      combine: {
-        files: {
-          'public/css/core.css': 'public/bower_components/bootstrap.css/css/bootstrap.css'
-        }
-      }
-    },
+	// Making grunt default to force in order not to break the project.
+	grunt.option('force', true);
 
-    less: {
-      options: {
-        //report:'gzip'
-      },
-      production: {
-        options: {
-          cleancss: true
-        },
-        files: {
-          "public/css/core.css": "public/bower_components/bootstrap/less/bootstrap.less"
-        }
-      }
-    },
+	// A Task for loading the configuration object
+	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
+		var init = require('./config/init')();
+		var config = require('./config/config');
 
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed'
-        },
-        files: {
-          'public/css/core.css': 'public/bower_components/sass-bootstrap/lib/bootstrap.scss',
-        }
-      }
-    },
+		grunt.config.set('applicationJavaScriptFiles', config.assets.js);
+		grunt.config.set('applicationCSSFiles', config.assets.css);
+	});
 
-    stylus: {
-      compile: {
-        options: {
-          compress:true
-        },
-        files: {
-          'public/css/core.css': 'public/bower_components/bootstrap-stylus/stylus/bootstrap.styl'
-        }
-      }
-    },
+	// Default task(s).
+	grunt.registerTask('default', ['lint', 'concurrent:default']);
 
-    concat: {
-      options: {
-        separator: ';',
-        stripBanners:true
-      },
-      dist: {
-        src: ['public/js/app.js'],
-        dest: 'public/js/concat.js',
-      },
-    },
+	// Debug task.
+	grunt.registerTask('debug', ['lint', 'concurrent:debug']);
 
-    //this is currently turned off, since jquery KILLS it
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: false,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      files:{
-        src:['public/js/concat.js']
-      }
-    },
+	// Secure task(s).
+	grunt.registerTask('secure', ['env:secure', 'lint', 'concurrent:default']);
 
-    uglify: {
-      options: {
-        mangle: false
-      },
-      my_target: {
-        files: {
-          'public/js/app.min.js': ['public/js/concat.js']
-        }
-      }
-    },
+	// Lint task(s).
+	grunt.registerTask('lint', ['jshint', 'csslint']);
 
-    // Watch Config
-    watch: {
-        files: ['views/**/*'],
-        options: {
-            livereload: true
-        },
-        scripts: {
-            files: [
-                'public/js/**/*.js'
-            ],
-            tasks:['build']
-        },
-        css: {
-            files: [
-                'public/css/**/*.css',
-            ],
-        },
-        less: {
-            files: ['public/bower_components/bootstrap/less/**/*.less'],
-            tasks: ['build']
-        },
-        express: {
-            files:  [ 'server.js', '!**/node_modules/**', '!Gruntfile.js' ],
-            tasks:  [ 'watch' ],
-            options: {
-                nospawn: true // Without this option specified express won't be reloaded
-            }
-        },
-    },
+	// Build task(s).
+	grunt.registerTask('build', ['lint', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
 
-    connect: {
-      options: {
-        port: RUNNING_PORT,//variable at top of this file
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, '.')
-            ];
-          }
-        }
-      }
-    },
-
-    nodemon:{
-      dev: {
-        options: {
-          file: 'server.js',
-          //args: ['dev'],
-          //nodeArgs: ['--debug'],
-          ignoredFiles: ['node_modules/**'],
-          //watchedExtensions: ['js'],
-          watchedFolders: ['views', 'routes'],
-          //delayTime: 1,
-          legacyWatch: true,
-          env: {
-            PORT: RUNNING_PORT
-          },
-          cwd: __dirname
-        }
-      }
-    },
-
-    // run 'watch' and 'nodemon' indefinitely, together
-    // 'launch' will just kick it off, and won't stay running
-    concurrent: {
-        target: {
-            tasks: ['nodemon', 'watch', 'launch'],
-            options: {
-                logConcurrentOutput: true
-            }
-        }
-    },
-
-    wait:{
-      options: {
-          delay: 1000
-      },
-      pause:{
-        options:{
-          before:function(options){
-            console.log('pausing %dms before launching page', options.delay);
-          },
-          after : function() {
-              console.log('pause end, heading to page (using default browser)');
-          }
-        }
-      }
-    },
-
-    open: {
-      server: {
-        path: 'http://localhost:' + RUNNING_PORT
-      }
-    }
-
-  });
-
-  //grunt.registerTask('server', ['build', 'connect:livereload', 'open', 'watch']);
-
-  grunt.registerTask('build', ['sass', 'concat', 'uglify']);
-
-  grunt.registerTask('launch', ['wait', 'open']);
-
-  grunt.registerTask('default', ['build', 'concurrent']);
-
+	// Test task.
+	grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
 };
--- server.js
+++ server.js
@@ -1,118 +1,36 @@
-/*************************************
-//
-// node-socket app
-//
-**************************************/
 'use strict';
+/**
+ * Module dependencies.
+ */
+var init = require('./config/init')(),
+	config = require('./config/config'),
+	chalk = require('chalk'),
+    r = require('rethinkdb');
 
-// express magic
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app)
-var io = require('socket.io').listen(server);
-var device = require('express-device');
-var r = require('rethinkdb');
-var connection = null;
+/**
+ * Main application entry file.
+ * Please note that the order of loading is important.
+ */
 
-var runningPortNumber = 80;
-
-app.configure(function () {
-    // I need to access everything in '/public' directly
-    app.use(express.static(__dirname + '/public'));
-
-    //set the view engine
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
-
-    app.use(device.capture());
+// Bootstrap db connection
+var db = r.connect(config.db, function(err) {
+	if (err) {
+		console.error(chalk.red('Could not connect to Rethinkdb!'));
+		console.log(chalk.red(err));
+	}
 });
 
-// logs every request
-app.use(function (req, res, next) {
-    // output every request in the array
-    console.log({
-        method: req.method,
-        url: req.url,
-        device: req.device
-    });
+// Init the express application
+var app = require('./config/express')(db);
 
-    // goes onto the next function in line
-    next();
-});
+// Bootstrap passport config
+require('./config/passport')();
 
-app.get('/', function (req, res) {
-    res.render('index', {});
-});
+// Start the app by listening on <port>
+app.listen(config.port);
 
-r.connect({
-    host: 'localhost',
-    port: 28015
-}, function (err, conn) {
-    if (err) {
-        throw err;
-    }
-    connection = conn;
-    r.table('member').changes().run(connection, function (err, cursor) {
-        if (err) {
-            throw err;
-        }
-        cursor.each(function (err, row) {
-            if (err) {
-                throw err;
-            }
-            console.log(JSON.stringify(row, null, 2));
-            io.sockets.emit('new-identified', {
-                name: row.new_val.name,
-                timestamp: new Date().getTime()
-            });
-        });
-    });
-});
+// Expose app
+exports = module.exports = app;
 
-io.sockets.on('connection', function (socket) {
-    r.table('member').run(connection, function (err, cursor) {
-        if (err) {
-            throw err;
-        }
-        cursor.toArray(function (err, result) {
-            if (err) {
-                throw err;
-            }
-            console.log(JSON.stringify(result, null, 2));
-            io.sockets.emit('new-identified', {
-                name: result[0].name,
-                timestamp: new Date().getTime()
-            });
-        });
-    });
-
-    socket.on('identified', function (data, fn) {
-        console.log(data);
-        r.table('member').update({
-            name: data.name
-        }).
-        run(connection, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            console.log(JSON.stringify(result, null, 2));
-            io.sockets.emit('blast', {
-                msg: '<span style=\"color:red !important\">' + data.name + ' connected</span>'
-            });
-            fn();
-        });
-    });
-
-    socket.on('blast', function (data, fn) {
-        console.log(data);
-        io.sockets.emit('blast', {
-            msg: data.msg
-        });
-
-        fn(); //call the client back to clear out the field
-    });
-
-});
-
-server.listen(runningPortNumber);
-console.log('Listening on port ' + runningPortNumber + '...');
+// Logging initialization
+console.log('REAN.JS application started on port ' + config.port);
