4a3068b13c0def08d73673c32e32b5fe9128f9eb
Fernando <fernando.rodriguez.adesis@gmail.com>
2015-08-14 15:55:47 +0200

    ADD First inclusion for typescript into project

---
+++ test/app/customer.js
@@ -0,0 +1,10 @@
+var Customer = (function () {
+    function Customer(name) {
+        this.name = name;
+    }
+    Customer.prototype.getName = function () {
+        return this.name;
+    };
+    return Customer;
+})();
+//# sourceMappingURL=customer.js.map
